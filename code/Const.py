ea6fd0d8a877ea21d2ac6b54de08cd229f17781b
Guilherme <gramoscampestrini@gmail.com>
2026-06-20 20:56:00 -0300

    Creating player shot Creating enemy shot

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 # C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
@@ -16,10 +15,15 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 5,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 5,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
     'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
+
 EVENT_ENEMY = pygame.USEREVENT + 1
 
 ENTITY_HEALTH = {
@@ -45,6 +49,13 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 20,
+    'Enemy1': 100,
+    'Enemy2': 200,
+}
+
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -61,7 +72,7 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
                    'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1:': pygame.K_RCTRL,
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
 # S
